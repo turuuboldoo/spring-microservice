cbcecf7b6f3b097c93ebb94f370c8b29fa1dcb1e
turuuboldoo <turuu.boldoo@gmail.com>
2022-08-18 11:07:34 +0800

    mysql to postgresql

--- gallery/build.gradle.kts
+++ gallery/build.gradle.kts
@@ -32,7 +32,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 
-    implementation("dev.miku:r2dbc-mysql:0.8.2.RELEASE")
+    runtimeOnly("org.postgresql:r2dbc-postgresql")
+    runtimeOnly("org.postgresql:postgresql")
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
--- sql/init.sql
+++ sql/init.sql
@@ -1,14 +1,14 @@
 create database if not exists gallery;
 create database if not exists image;
 
-create table gallery.galleries
+create table galleries
 (
     id           SERIAL PRIMARY KEY,
     title        VARCHAR(255) NOT NULL,
     description  VARCHAR(255) NOT NULL
 );
 
-INSERT INTO gallery.galleries (title, description)
+INSERT INTO galleries (title, description)
 VALUES ('birthday', 'birthday at work'),
        ('vacation', 'hawaii island was amazing'),
        ('nba finals', 'nba finals 2021-2022');
