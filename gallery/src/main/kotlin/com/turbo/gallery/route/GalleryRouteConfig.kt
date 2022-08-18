d419e3e2512d271a628c073b6d207636e2fcb75f
amartuvshinBayarmaa <amartuvshinBaya@gmail.com>
2022-08-18 11:27:30 +0800

    wip

--- gallery/src/main/kotlin/com/turbo/gallery/route/GalleryRouteConfig.kt
+++ gallery/src/main/kotlin/com/turbo/gallery/route/GalleryRouteConfig.kt
@@ -3,6 +3,7 @@ package com.turbo.gallery.route
 import com.turbo.gallery.handler.GalleryHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.server.coRouter
 
 @Configuration
@@ -13,6 +14,13 @@ class GalleryRouteConfig(
     @Bean
     fun routes() = coRouter {
         GET("/", handler::getRoot)
-        GET("api/gallery", handler::getGalleries)
+
+        "/api/gallery".nest {
+            accept(MediaType.APPLICATION_JSON).nest {
+                GET("", handler::getGalleries)
+                GET("/{id}", handler::getGalleries)
+                POST("", handler::getGalleries)
+            }
+        }
     }
 }
