6c2d5d770316917dfd358b2df86a9e853b1a3bd7
turuuboldoo <turuu.boldoo@gmail.com>
2022-08-19 12:58:54 +0800

    keyclaok oauth2 added

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
 
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot:spring-boot-starter-security")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
--- gateway/src/main/kotlin/com/turbo/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/com/turbo/gateway/GatewayApplication.kt
@@ -3,6 +3,11 @@ package com.turbo.gateway
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -10,4 +15,27 @@ class GatewayApplication
 
 fun main(args: Array<String>) {
     runApplication<GatewayApplication>(*args)
+}
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(httpSecurity: ServerHttpSecurity): SecurityWebFilterChain {
+        httpSecurity
+            .csrf()
+            .disable()
+            .authorizeExchange { exchange ->
+                exchange.pathMatchers("/eureka/**")
+                    .permitAll()
+                    .anyExchange()
+                    .authenticated()
+            }
+            .oauth2ResourceServer { serverSpec ->
+                serverSpec.jwt()
+            }
+
+        return httpSecurity.build()
+    }
 }
