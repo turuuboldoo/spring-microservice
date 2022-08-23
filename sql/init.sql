f6aaac608cc9581e2c936c23db955a8db91ec5c5
turuuboldoo <turuu.boldoo@gmail.com>
2022-08-23 17:07:15 +0800

    gallery in memory db test

--- sql/init.sql
+++ sql/init.sql
@@ -1,14 +1,14 @@
 create database if not exists gallery;
 create database if not exists image;
 
-create table galleries
+create table gallery.galleries
 (
     id           SERIAL PRIMARY KEY,
     title        VARCHAR(255) NOT NULL,
     description  VARCHAR(255) NOT NULL
 );
 
-INSERT INTO galleries (title, description)
+INSERT INTO gallery.galleries (title, description)
 VALUES ('birthday', 'birthday at work'),
        ('vacation', 'hawaii island was amazing'),
        ('nba finals', 'nba finals 2021-2022');
